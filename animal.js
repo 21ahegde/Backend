533f7690edded9c5ddc0e7d94397cc2407daa1d7
Aaron Hegde <hegde.aaron@gmail.com>
2020-04-07 18:39:20 -0400

    almost there

--- animal.js
+++ animal.js
@@ -2,14 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3001
 const fs = require('fs');
+const path = require('path'); 
 
 
 
-app.use('/static', express.static((__dirname, 'public')))
+app.use(express.static(__dirname + '/public')); 
 
 app.get('/random', (req, res) => {
-    const files = fs.readdirSync((__dirname, 'public')) 
-    res.send({ message: 'http://localhost:3001/public/' + files[0], status: "OK"}) 
+    const files = fs.readdirSync(__dirname + '/public');
+     const filterFiles = files.filter(fileName => path.extName(fileName) === '.jpg')
+    res.send('http://localhost:3001/public/' + filterFiles[getRandomInt(0, filterFiles.length)]) 
 })
 
 app.listen(port, () => console.log(`Looking for dog at http://localhost:${port}`))
+
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
